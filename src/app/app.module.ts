98cea58498f5a7ad9941467ba8ef418676ae9fdc
Matheus Valandro <msvalandro@gmail.com>
2019-11-10 09:09:56 -0300

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './modules/home/home.component';
+import { NavbarComponent } from './modules/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
